0ad1389779e111fa619e42f5df4a11d5bb39c101
MariaSchweren <maria2612@t-online.de>
2024-03-10 23:37:44 +0100

    working

--- MetalScraper.py
+++ MetalScraper.py
@@ -2,31 +2,33 @@ import urllib
 from bs4 import BeautifulSoup
 import requests
 
+Linklist = open("Linklist.txt")                                                             #öffnet Datei Linklist 
+Datalist = open("Datalist.csv", "a", encoding="utf-8")                                      #öffnet Datalist mti allen gescrapten Links, als "nur-schreiben", und utf8-encoded um Sonderzeichen zu encoden, weil vorher unicode genutzt wurde
 
-Linklist = open("Linklist.txt")
-Datalist = open("Datalist.csv", "w")
+for line in Linklist:                                                                                        #für jeden Link aus Linklist:
+    page = requests.get(line, headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"})           #get request an website vom jeweiligen link
 
-for line in Linklist:
-    page = requests.get(line, headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"})
+    if page.status_code == 200:                                                                                 #200 status = website gibt ok für request
+        content = page.content                                                                                  
 
-    if page.status_code == 200:
-        content = page.content
-
-    Data = {}
-    DOMdocument = BeautifulSoup(content, 'html.parser')
+    Data = {}                                                                                                   #überflüssig
+    DOMdocument = BeautifulSoup(content, 'html.parser')                                                         #parsen von website content mit bs4
 
     #title = DOMdocument.title.string 
-    name = DOMdocument.find("h1", class_='band_name')
+    name = DOMdocument.find("h1", class_='band_name')                                                           #suche nach Name im html dok
     
-    logo_prior = DOMdocument.find("a",  id="logo")
-    if logo_prior  != None:
-        logo = logo_prior.attrs['href']
+    logo_prior = DOMdocument.find("a",  id="logo")                                                              #suche nach logo im html dok / find gibt 'None' zurück, wenn es nichts findet
+    if logo_prior  != None:                                                                                     #falls Logo vorhanden
+        logo = logo_prior.attrs['href']                                                                         #dann wird link von href geholt
     else:
-        continue
+        print(f"{name} has no logo!")                                                                           #sonst, gib über print aus, dass diese Band kein Logo hat
+        continue                                                                                                #überspringt Bands ohne Logo -> springt zum Schleifenbeginn und setzt fort
+   # print(name)
+    #logo = DOMdocument.find("a",  id="logo")['href']
 
-    Genre = DOMdocument.find_all(['dl','dd'], {'class': "float_right"})
-    for div in Genre:
-        genre = div.text.split("\n")[2]
+    Genre = DOMdocument.find_all(['dl','dd'], {'class': "float_right"})                                         #Genre in html finden -> war über d1 & dd markiert, float right = Seitenaufbau frontend (genre steht rechts) 
+    for div in Genre:                                                                                           #find_all gibt iterable zurück, nur mit 1 Eintrag 
+        genre = div.text.split("\n")[2]                                                                         #nimmt den EIntrag wo das Genre drin steht und liefert text zurück, der von \n umschlossen ist
 
 
 
---
+++ Picturedownload.py
@@ -0,0 +1,28 @@
+import os, requests
+#Datalist.csv einlesen 
+
+Datalist = open("Bsplist.csv", encoding="utf-8")
+#for schleife für alle Einträge in Datalist
+for entry in Datalist:    
+    #über split die Einträge nach ; parsen 
+    entryArr = entry.split(";")
+    #Array kommt zurück -> Name Genre Link
+    bandName = entryArr[0]
+    genre = entryArr[1]
+    logo = entryArr[2].rstrip("\n")
+    # Genre splitten nach / 
+    genreArr = genre.split("/")
+    for genre in genreArr:
+        #Ordnerstruktur anlegen -> schauen ob es Ordner für Genre aus Array schon gibt
+        if not os.path.exists(f"Genres\{genre}"):
+            os.makedirs(f"Genres\{genre}")          #wenn es das gibt - nehmen, wenn nicht - Ordner anlegen
+         #wenn Ordner -> Datei runterladen und in Ordner speichern
+        reqAnswer = requests.get(logo, headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}, stream = True)
+        if reqAnswer.status_code != 200: # Eventuell Datei nicht gefunden oder so
+            print("download failed", bandName)
+        fileName = bandName + "_" + logo.split("/")[-1].split("?")[0] # Dateiname wird aus Bandname und Name aus Logolink zusammengebastelt: /284593.jpg?124 -> bla_284593.jpg
+        print(fileName)
+        # lade Datei strückweise herunter um den Arbeitsspeicher zu schonen
+        with open(f"Genres\{genre}\{fileName}", "wb") as f:
+            for chunk in reqAnswer.iter_content(1024):
+                f.write(chunk)
