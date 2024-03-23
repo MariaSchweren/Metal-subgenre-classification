1efdd99ac685cc8fd3a3d8b88ad04e59f8a627b1
MariaSchweren <Maria2612@t-online.de>
2024-03-23 17:18:17 +0100

    Exceptions

--- Pictureconverter.py
+++ Pictureconverter.py
@@ -1,25 +1,35 @@
 from PIL import Image
 import os
 
+# from https://pythonhint.com/post/6698662917296068/pil-convert-gif-frames-to-jpg
+# and https://pythonexamples.org/python-check-if-path-is-file-or-directory/
+
 dataPath = 'Genres'
 
-for subdir in os.listdir(dataPath):
-    current_path = os.path.join(dataPath, subdir)
-    for file in os.listdir(current_path):
-        if os.path.isfile(os.path.join(current_path, file)): # exclude folders
-            if file[-3:] == "gif":
-                with Image.open(os.path.join(current_path, file)) as im:
-                    for frame_num in range(im.n_frames):
-                        # select the current frame
-                        im.seek(frame_num)
+try:
+    for subdir in os.listdir(dataPath):
+        current_path = os.path.join(dataPath, subdir)
+        for file in os.listdir(current_path):
+            if os.path.isfile(os.path.join(current_path, file)): # exclude folders
+                if file[-3:] == "gif":
+                    with Image.open(os.path.join(current_path, file)) as im:
+                        if im.format == "GIF":
+                            for frame_num in range(im.n_frames):
+                                # select the current frame
+                                im.seek(frame_num)
 
-                        # create a new JPEG image with the same size and mode as the GIF frame
-                        jpeg_frame = Image.new('RGB', im.size)
+                                # create a new JPEG image with the same size and mode as the GIF frame
+                                jpeg_frame = Image.new('RGB', im.size)
 
-                        # paste the current frame onto the jpeg image
-                        jpeg_frame.paste(im)
+                                # paste the current frame onto the jpeg image
+                                jpeg_frame.paste(im)
 
-                        # save the jpeg image with a filename that includes the current frame number
-                        jpeg_filename = str(file).replace(".gif",".jpg")
-                        jpeg_frame.save(os.path.join(current_path, jpeg_filename))
-                        break
+                                # save the jpeg image with a filename that includes the current frame number
+                                jpeg_filename = str(file).replace(".gif",".jpg")
+                                jpeg_frame.save(os.path.join(current_path, jpeg_filename))
+                                break
+                        else: # format is already jpg
+                            jpeg_filename = str(file).replace(".gif",".jpg")
+                            jpeg_frame.save(os.path.join(current_path, jpeg_filename))
+except Exception as e:
+    print(e)
