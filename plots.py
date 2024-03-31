5fccc23d799d7dcf340f2601ee1dbd7f0e16fd52
MariaSchweren <Maria2612@t-online.de>
2024-03-31 23:24:41 +0200

    last update

--- plots.py
+++ plots.py
@@ -5,7 +5,8 @@ genre = ["Ambient", "Atmospheric Black", "Black", "Brutal Death", "Death",
          "Hardcore (Punk)", "Heavy", "Metalcore","Power", "Sludge Metal", "Trash", " Speed", "Stoner"]
 samples = [154, 211, 2597, 301, 2309, 178, 856, 232, 278, 483, 593, 393, 172, 1952, 407, 459, 383, 2196, 200, 336]
     #plot diagram 
-plt.bar(range(len(genre)), samples)
+plt.bar(range(len(genre)), samples, width=0.5)
+plt.xticks(rotation=90)
 
 plt.title("Samples per Subgenre")
 plt.ylabel("Number of samples")
